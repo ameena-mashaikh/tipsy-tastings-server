651a0d9effb1660c8f40e5f682f7432578b27f52
Ameena Mashaikh <ameenamashaikh@gmail.com>
2022-12-05 16:31:00 -0500

    halfway through models

---
+++ tipsytastingapi/models/cocktail.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Cocktail(models.Model):
+    name = models.CharField(max_length = 75)
+    category = models.ForeignKey("Category" on_delete=models.CASCADE, related_name='cocktail_category')
+    recipe = models.CharField(max_length = 800)
+    #image = models.CharField(max_length=513) OR models.URLField(max_length=300, blank=True)
+    mixologist = models.ForeignKey("Mixologist" on_delete=models.CASCADE, related_name='cocktail_mixologist')
