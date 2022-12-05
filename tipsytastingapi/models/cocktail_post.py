90145b37ae48b2be735d7129438fce85aa1dbaa1
Ameena Mashaikh <ameenamashaikh@gmail.com>
2022-12-05 16:48:40 -0500

    finished up the models

--- tipsytastingapi/models/cocktail_post.py
+++ tipsytastingapi/models/cocktail_post.py
@@ -3,4 +3,4 @@ from django.db import models
 class CocktailPost(models.Model):
     name = models.CharField(max_length = 75)
     cocktail = models.ForeignKey("Cocktail", on_delete=models.CASCADE, related_name='post_cocktail')
-    caption = models.CharField(max_length = 75)
+    caption = models.CharField(max_length = 200)
--- tipsytastingapi/models/staple_ingredients.py
+++ tipsytastingapi/models/staple_ingredients.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-class CommonIngredients(models.Model): 
+class StapleIngredients(models.Model): 
     name = models.CharField(max_length=75)
     #image = models.CharField(max_length=513) OR models.URLField(max_length=300, blank=True)
